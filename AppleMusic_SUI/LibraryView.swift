1824fcfa65d5d1ab9b639180250aeaaf96164b35
golikovartyom <golikovartem404@gmail.com>
2022-11-26 16:47:27 +0400

    Add LibraryView

---
+++ AppleMusic_SUI/LibraryView.swift
@@ -0,0 +1,31 @@
+//
+//  LibraryView.swift
+//  AppleMusic_SUI
+//
+//  Created by User on 26.11.2022.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Search your music?")
+                    .font(.system(size: 25, weight: .bold, design: .default))
+                Text("All your music which you buy in iTunes Store stores here")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            .frame(width: 250)
+            .multilineTextAlignment(.center)
+            .navigationTitle("Library")
+        }
+    }
+}
+
+struct LibraryView_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryView()
+    }
+}
