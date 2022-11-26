2f30715bf3f245610a732ba3cd22dca1e29eaa1b
golikovartyom <golikovartem404@gmail.com>
2022-11-26 16:39:45 +0400

    Initial Commit

---
+++ AppleMusic_SUI/AppleMusic_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_SUIApp.swift
+//  AppleMusic_SUI
+//
+//  Created by User on 26.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
