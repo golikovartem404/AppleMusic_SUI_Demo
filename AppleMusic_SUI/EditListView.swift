693a83743ca02c2a7a88114fcd3496cde6e9518b
golikovartyom <golikovartem404@gmail.com>
2022-11-26 16:52:59 +0400

    Add cell model

---
+++ AppleMusic_SUI/EditListView.swift
@@ -0,0 +1,20 @@
+//
+//  EditListView.swift
+//  AppleMusic_SUI
+//
+//  Created by User on 26.11.2022.
+//
+
+import SwiftUI
+
+struct EditListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditListView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditListView()
+    }
+}
