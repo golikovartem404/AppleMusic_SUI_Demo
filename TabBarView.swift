8a4de69e0150e4fa90646f1b9c00989727741471
golikovartyom <golikovartem404@gmail.com>
2022-11-26 17:45:01 +0400

    Add TabBarView

---
+++ TabBarView.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarView.swift
+//  AppleMusic_SUI
+//
+//  Created by User on 26.11.2022.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
