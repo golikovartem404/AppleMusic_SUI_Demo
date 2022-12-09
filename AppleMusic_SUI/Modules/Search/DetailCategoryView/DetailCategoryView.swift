4471ce8433275e69c7b727685cf902659a775867
golikovartyom <golikovartem404@gmail.com>
2022-12-09 18:48:46 +0400

    Add assets, configure model and setup structure

---
+++ AppleMusic_SUI/Modules/Search/DetailCategoryView/DetailCategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailCategoryView.swift
+//  AppleMusic_SUI
+//
+//  Created by User on 09.12.2022.
+//
+
+import SwiftUI
+
+struct DetailCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailCategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailCategoryView()
+    }
+}
