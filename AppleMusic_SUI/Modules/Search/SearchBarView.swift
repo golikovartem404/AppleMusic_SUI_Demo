4fb7e09c2113f168ae4af9b173783b15fe9d6d3a
golikovartyom <golikovartem404@gmail.com>
2022-12-11 12:30:28 +0400

    Configure search bar

--- AppleMusic_SUI/Modules/Search/SearchBarView.swift
+++ AppleMusic_SUI/Modules/Search/SearchBarView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct SearchBarView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @Binding var searchText: String
+    @Binding var isSearching: Bool
 
-struct SearchBarView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchBarView()
+    var body: some View {
+        HStack {
+            HStack(spacing: 8) {
+                Image(systemName: "magnifyingglass")
+                    .font(.system(size: 18))
+                    .foregroundColor(Color(.systemGray3))
+                    .padding(.leading, 12)
+
+                TextField("Your library", text: $searchText)
+                    .onTapGesture {
+                        isSearching = true
+                    }
+
+                if isSearching {
+                    Button(action: { searchText = "" }, label: {
+                        Image(systemName: "xmark.circle.fill")
+                    })
+                    .foregroundColor(Color(.systemGray3))
+                    .padding(.trailing, 12)
+                }
+            }
+            .frame(height: 40)
+            .background(Color.init(.systemGray6))
+            .cornerRadius(15)
+            .padding(.top, 5)
+            .padding([.leading, .trailing], 12)
+
+            if isSearching {
+                Button {
+                    searchText = ""
+                    isSearching = false
+                    UIApplication.shared.sendAction(
+                        #selector(UIResponder.resignFirstResponder),
+                        to: nil,
+                        from: nil,
+                        for: nil
+                    )
+                } label: {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 12)
+            }
+        }
+        .transition(.move(edge: .trailing))
+        .animation(.linear, value: 1)
     }
 }
