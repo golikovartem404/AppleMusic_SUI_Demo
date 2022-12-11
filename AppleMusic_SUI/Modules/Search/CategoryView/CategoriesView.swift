7d000383feb708f55ed84da55e6f02c95ab13d0f
golikovartyom <golikovartem404@gmail.com>
2022-12-11 12:33:58 +0400

    Reconfigure search view and add categories view

---
+++ AppleMusic_SUI/Modules/Search/CategoryView/CategoriesView.swift
@@ -0,0 +1,48 @@
+//
+//  CategoriesView.swift
+//  AppleMusic_SUI
+//
+//  Created by User on 11.12.2022.
+//
+
+import SwiftUI
+
+struct CategoriesView: View {
+    @State var categories = CategoryModel.categories
+
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                Section {
+                    ForEach(categories, id: \.id) { category in
+                        NavigationLink(destination: DetailCategoryView()) {
+                            CategoryItemView(
+                                categoryImage: category.categoryImage,
+                                categoryTitle: category.categoryTitle
+                            )
+                        }
+                    }
+                } header: {
+                    Text("Search by category")
+                        .font(.system(size: 23, weight: .black, design: .default))
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top, 10)
+                }
+
+            }
+        }
+        .padding([.leading, .trailing], 12)
+    }
+}
+
+struct CategoriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoriesView()
+    }
+}
--- AppleMusic_SUI/Modules/Search/SearchView.swift
+++ AppleMusic_SUI/Modules/Search/SearchView.swift
@@ -10,51 +10,14 @@ import SwiftUI
 struct SearchView: View {
     @State private var isSearching = false
     @State private var searchText = ""
-    @State var categories = CategoryModel.categories
-
-    let columns: [GridItem] = [
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
 
     var body: some View {
         NavigationView {
             VStack {
-                HStack {
-                    Image(systemName: "magnifyingglass")
-                        .font(.system(size: 20))
-                        .foregroundColor(.gray)
-                        .padding(.leading, 10)
-                    TextField("Your library", text: $searchText)
+                SearchBarView(searchText: $searchText, isSearching: $isSearching)
+                if !isSearching {
+                    CategoriesView()
                 }
-                .frame(height: 40)
-                .background(Color.init(UIColor.systemGray5))
-                .cornerRadius(12)
-                .padding(.top, 4)
-                .padding([.leading, .trailing], 12)
-
-                ScrollView {
-                    LazyVGrid(columns: columns) {
-                        Section {
-                            ForEach(categories, id: \.id) { category in
-                                NavigationLink(destination: DetailCategoryView()) {
-                                    CategoryItemView(
-                                        categoryImage: category.categoryImage,
-                                        categoryTitle: category.categoryTitle
-                                    )
-                                }
-                            }
-                        } header: {
-                            Text("Search by category")
-                                .font(.system(size: 23, weight: .black, design: .default))
-                                .fontWeight(.bold)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.top, 10)
-                        }
-                        
-                    }
-                }
-                .padding([.leading, .trailing], 12)
             }
             .navigationTitle("Search")
         }
