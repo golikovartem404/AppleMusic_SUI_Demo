faa7e61ff3a068e759d1fd869bf1e01f52f976df
golikovartyom <golikovartem404@gmail.com>
2022-12-11 12:16:07 +0400

    Configure structure

---
+++ AppleMusic_SUI/Modules/Search/SearchResultsView/AppleMusicLibraryView.swift
@@ -0,0 +1,20 @@
+//
+//  AppleMusicLibraryView.swift
+//  AppleMusic_SUI
+//
+//  Created by User on 11.12.2022.
+//
+
+import SwiftUI
+
+struct AppleMusicLibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppleMusicLibraryView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppleMusicLibraryView()
+    }
+}
---
+++ AppleMusic_SUI/Modules/Search/SearchResultsView/SearchResultsView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultsView.swift
+//  AppleMusic_SUI
+//
+//  Created by User on 11.12.2022.
+//
+
+import SwiftUI
+
+struct SearchResultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchResultsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchResultsView()
+    }
+}
